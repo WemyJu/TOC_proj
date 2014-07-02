48b0b700aa8abec7d4b33737ae3813bb83ce3670
WemyJu <wemyju@gmail.com>
2014-07-02 12:02:09 +0800

    fix the functions of regression

--- lib/Regression.py
+++ lib/Regression.py
@@ -1,39 +1,54 @@
 from numpy import *
+import statsmodels.api as sm
 
 class Regression():
 	def __init__(self):
 		self.statData = array([])
+		self.totalPrice = array([])
+		self.parameters = array([])
 
-	def quantize(self, rawData)
-		for city in rawData:
-			#if city not in statData:
-				#statData[city] = {}
-			for block in rawData[city]:
-				#if block not in statDaty[city]:
-					#statData[city][block] = {}
-				for record in rawData[city][block]:
-					x1 = 0
-					x2 = (record['有無管理組織'] == '有')
-					x3 = record['土地轉移面積']
-					x4 = record['車位轉移面積']
-					x5 = record['建物轉移面積']
-					if record['建築完成年月'] != None:
-						houseAge = 1031231 - record['建築完成年月']
-						x6 = (houseAge/10000)*12 + (houseAge%10000)/100
-					else:
-						houseAge = 10312 - record['交易年月']
-						x6 = (houseAge/100)*12 + houseAge%100
-					x7 = (record['建物型態'] == '住宅大樓(11層含以上有電梯)')
-					x8 = (record['建物型態'] == '套房(1房1廳1衛)')
-					x9 = (record['建物型態'] == '華廈(10層含以下有電梯)')
-					x10 = (record['建物型態'] == '公寓(5樓含以下無電梯)')
-					x11 = (record['建物型態'] == '透天厝')
-					x12 = (record['建物型態'] == '店面(店鋪)')
-					
-					if len(self.statData):
-						self.statData = vstack((self.statData, [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12]))
-					else:
-						self.statData = array([x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12])
+	def quantize(self, rawRecord):
+		x1 = 0
+		x2 = (rawRecord['有無管理組織'] == '有')
+		x3 = rawRecord['土地移轉總面積平方公尺']
+		x4 = rawRecord['車位移轉總面積平方公尺']
+		x5 = rawRecord['建物移轉總面積平方公尺']
+		if rawRecord['建築完成年月'] != None:
+			houseAge = 1031231 - rawRecord['建築完成年月']
+			x6 = (houseAge/10000)*12 + (houseAge%10000)/100
+		else:
+			houseAge = 10312 - rawRecord['交易年月']
+			x6 = (houseAge/100)*12 + houseAge%100
+		x7 = (rawRecord['建物型態'] == '住宅大樓(11層含以上有電梯)')
+		x8 = (rawRecord['建物型態'] == '套房(1房1廳1衛)')
+		x9 = (rawRecord['建物型態'] == '華廈(10層含以下有電梯)')
+		x10 = (rawRecord['建物型態'] == '公寓(5樓含以下無電梯)')
+		x11 = (rawRecord['建物型態'] == '透天厝')
+		x12 = (rawRecord['建物型態'] == '店面(店鋪)')
+		y = rawRecord['總價元']
+		
+		if len(self.statData):
+			self.statData = vstack((self.statData, [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12]))
+		else:
+			self.statData = array([x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12])
+			
+		if len(self.totalPrice):
+			self.totalPrice = hstack((self.totalPrice, [y]))
+		else:
+			self.totalPrice = array([y])
+                            
+	def countRegression(self):
+		if len(self.totalPrice) > 5:
+			self.statData = sm.add_constant(self.statData, prepend=False)
+			model = sm.OLS(self.totalPrice, self.statData)
+			results = model.fit()
+			self.parameters = results.params
+			print(self.parameters)
+			return True
+			#print(results.summary())
+		else:
+			return False
 
-	def getStatData(self):
-		return self.statData
+	def getParameters(self):
+		print(self.parameters)
+		return self.parameter
--- real_price_predictor.py
+++ real_price_predictor.py
@@ -1,5 +1,6 @@
 from lib.DataGarageAPI import DataGarageAPI
 from lib.addressClassifier import AddressClassifier
+from lib.Regression import Regression
 # from operator import itemgetter
 # from lib.myStatistics import MyStatistics
 
@@ -7,20 +8,33 @@ Block = {}
 
 
 if __name__ == '__main__':
-    dgAPI = DataGarageAPI()
-    dgAPI.setDataID('5365dee31bc6e9d9463a0057') \
-         .setSelector([['都市土地使用分區', '=', '住']]) \
-         .setFields(['土地區段位置或建物區門牌', '鄉鎮市區',
+	dgAPI = DataGarageAPI()
+	dgAPI.setDataID('5365dee31bc6e9d9463a0057') \
+		 .setSelector([['都市土地使用分區', '=', '住']]) \
+		 .setFields(['土地區段位置或建物區門牌', '鄉鎮市區',
                     '總價元', '有無管理組織', '建物型態',
                     '土地移轉總面積平方公尺', '車位移轉總面積平方公尺',
                     '建物移轉總面積平方公尺', '建物型態', '建築完成年月', '交易年月'])
 
-    data = dgAPI.getFilteredData()
+	data = dgAPI.getFilteredData()
 
-    ac = AddressClassifier()
-    ac.classify(data)
-    tempData = ac.getClassifiedData()
-    for i in tempData:
-        for j in tempData[i]:
-            for k in tempData[i][j]:
-                print (k)
+	ac = AddressClassifier()
+	ac.classify(data)
+	rawData = ac.getClassifiedData()
+	for city in rawData:
+		for block in rawData[city]:
+			dfs = Regression()
+			for record in rawData[city][block]:
+#print(record)
+				dfs.quantize(record)
+			print(city, block)
+			if dfs.countRegression():
+				parameters = dfs.getParameters
+				#print(parameters, "\n")
+            #print(parameters[0])
+			#print("y = ", parameters[0], end = "")
+				for i in range(1, 12, 1):
+					print((" + ", parameters[i], "x", str(i)), end = "")
+				print("\n")
+			else:
+				print("  The records of data are too insufficient to get regression.\n")
