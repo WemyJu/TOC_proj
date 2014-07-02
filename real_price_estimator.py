fb8b4692623b3205be060ad786ece7e15294f88e
LeeW <cl87654321@gmail.com>
2014-07-03 04:11:35 +0800

    finish estimator

--- real_price_estimator.py
+++ real_price_estimator.py
@@ -19,34 +19,36 @@ def readRegressions(path="regression_output/.regressions"):
 
 def userInput():
     inputVars = {}
-    inputVars['土地區段位置或建物區門牌'] = input('土地區段位置或建物區門牌')
-    inputVars['鄉鎮市區'] = input('鄉鎮市區')
-    inputVars['有無管理組織'] = input('有無管理組織')
-    inputVars['建物型態'] = input('建物型態')
-    inputVars['土地移轉總面積平方公尺'] = input('土地移轉總面積平方公尺')
-    inputVars['車位移轉總面積平方公尺'] = input('車位移轉總面積平方公尺')
-    inputVars['建物移轉總面積平方公尺'] = input('建物移轉總面積平方公尺')
-    inputVars['建築完成年月'] = input('屋齡')
-    inputVars['交易年月'] = input('交易年月')
+    inputVars['土地區段位置或建物區門牌'] = input('土地區段位置或建物區門牌 : ')
+    inputVars['鄉鎮市區'] = input('鄉鎮市區 : ')
+    inputVars['有無管理組織'] = input('有無管理組織 : ')
+    inputVars['建物型態'] = input('建物型態 : ')
+    inputVars['土地移轉總面積平方公尺'] = float(input('土地移轉總面積平方公尺 : '))
+    inputVars['車位移轉總面積平方公尺'] = float(input('車位移轉總面積平方公尺 : '))
+    inputVars['建物移轉總面積平方公尺'] = float(input('建物移轉總面積平方公尺 : '))
+    inputVars['建築完成年月'] = int(input('建築完成日期 : '))
+    inputVars['交易年月'] = int(input('交易年月 : '))
     return inputVars
 
 
 def quantize(data):
+    city = data['土地區段位置或建物區門牌'][:3]
+    region = data['鄉鎮市區']
+
     reg = Regression()
     result = list()
-    result.append(data['土地區段位置或建物區門牌'][:3])
-    result.append(data['鄉鎮市區'])
-    result.extend(reg.quantize())
+    result.append(city)
+    result.append(region)
+    result.extend(reg.quantizeForRec(data, city))
     return result
 
 
 def estimate(var, regs):
-    firstLevel = var[0]
-    secondLevel = var[1]
+    city = var[0]
+    region = var[1]
 
-    reg = regs[firstLevel][secondLevel]
-
-    y = numpy.dot(var[2:], reg[1:]) + reg[0]
+    reg = regs[city][region]
+    y = numpy.dot(numpy.array(var[2:]), reg)
     return y
 
 
@@ -55,6 +57,5 @@ if __name__ == '__main__':
     quantizedVars = quantize(inputVars)
 
     regressions = readRegressions()
-
     predictedPrice = estimate(quantizedVars, regressions)
     print ("The predicted price is : ", predictedPrice)
