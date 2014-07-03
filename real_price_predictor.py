91126aad70201cf027a4c645a43d77993918d10e
LeeW <cl87654321@gmail.com>
2014-07-03 13:49:32 +0800

    separate functions in predictor as userinterface

--- real_price_predictor.py
+++ real_price_predictor.py
@@ -7,124 +7,121 @@ from distutils.util import strtobool
 from lib.Regression import Regression
 
 
-inputTitle = ['縣市', '鄉鎮市區',
-              '有無管理組織', '建物型態',
-              '土地移轉總面積平方公尺', '車位移轉總面積平方公尺', '建物移轉總面積平方公尺',
-              '建築完成年月', '交易年月']
+class userInterface():
+    def __init__(self):
+        self.inputTitle = ['縣市', '鄉鎮市區',
+                           '有無管理組織', '建物型態',
+                           '土地移轉總面積平方公尺', '車位移轉總面積平方公尺', '建物移轉總面積平方公尺',
+                           '建築完成年月', '交易年月']
+        self.defaultPath = "regression_output"
+        self.regressions = dict()
+        self.inputVars = dict()
+        self.quantizedVars = dict()
+        self.predictPrice = int()
 
+        self.readRegressions()
 
-def readRegressions(path="regression_output/.regressions"):
-    if not os.path.isfile(path):
-        cmd = ["python3", "regression_generator.py"]
-        p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        p.communicate()
+    def readRegressions(self, path=None):
+        path = self.defaultPath + "/.regressions" if path is None else 0
+        if not os.path.isfile(path):
+            print ("There is not regressions. Automatically generate.")
+            cmd = ["python3", "regression_generator.py"]
+            p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            p.communicate()
+            print ("Successfully generate!")
 
-    f = open(path, "rb")
-    regressions = pickle.load(f)
-    f.close()
-    return regressions
+        f = open(path, "rb")
+        self.regressions = pickle.load(f)
+        f.close()
 
+    def yesNoQuery(self, question):
+        print(question, end = "\t")
+        while True:
+            try:
+                return strtobool(input().lower())
+            except ValueError:
+                print ("Please repond with y or n")
 
-def yesNoQuery(question):
-    print(question, end = "\t")
-    while True:
+    def __isfloat(self, inStr):
         try:
-            return strtobool(input().lower())
+            float(inStr)
+            return True
         except ValueError:
-            print ("Please repond with y or n")
+            return False
 
+    def __handleInputType(self, inStr):
+        if inStr.isnumeric():
+            inStr = int(inStr)
+        elif self.__isfloat(inStr):
+            inStr = float(inStr)
+        return inStr
 
-def isfloat(inStr):
-    try:
-        float(inStr)
-        return True
-    except ValueError:
-        return False
+    def userInput(self):
+        for title in self.inputTitle:
+            self.inputVars[title] = self.__handleInputType(input(title + ":"))
 
+    def fileInput(self, path):
+        try:
+            f = open(path, "r")
 
-def handleInputType(inStr):
-    if inStr.isnumeric():
-        inStr = int(inStr)
-    elif isfloat(inStr):
-        inStr = float(inStr)
-    return inStr
+            for line, title in zip(f.read().splitlines(), self.inputTitle):
+                self.inputVars[title] = self.__handleInputType(line)
 
-
-def userInput():
-    inputVars = dict()
-
-    global inputTitle
-    for title in inputTitle:
-        inputVars[title] = handleInputType(input(title + ":"))
-    return inputVars
-
-
-def fileInput(path):
-    inputVars = dict()
-
-    global inputTitle
-    try:
-        f = open(path, "r")
-
-        for line, title in zip(f.read().splitlines(), inputTitle):
-            inputVars[title] = handleInputType(line)
-
-        if len(inputVars) != len(inputTitle):
-            print ("Your input file contains wrong parameter numbers!!!")
+            if len(self.inputVars) != len(self.inputTitle):
+                print ("Your input file contains wrong parameter numbers!!!")
+                sys.exit(0)
+        except FileNotFoundError:
+            print ("Your input file does not exist!!!")
             sys.exit(0)
-    except FileNotFoundError:
-        print ("Your input file does not exist!!!")
-        sys.exit(0)
-    return inputVars
 
+    def quantize(self):
+        city = self.inputVars['縣市'][:3]
+        region = self.inputVars['鄉鎮市區']
 
-def quantize(data):
-    city = data['縣市'][:3]
-    region = data['鄉鎮市區']
+        reg = Regression()
+        self.quantizedVars['縣市'] = city
+        self.quantizedVars['鄉鎮市區'] = region
+        self.quantizedVars['variables'] = numpy.array(reg.quantizeForRec(self.inputVars, city))
+        return self
 
-    reg = Regression()
-    result = dict()
-    result['縣市'] = city
-    result['鄉鎮市區'] = region
-    result['variables'] = numpy.array(reg.quantizeForRec(data, city))
-    return result
+    def predict(self):
+        city = self.quantizedVars['縣市']
+        region = self.quantizedVars['鄉鎮市區']
+        coefficient = self.regressions[city][region]
+        self.predictedPrice = numpy.dot(self.quantizedVars["variables"], coefficient)
 
+    def getPredictedPrice(self):
+        return self.predictedPrice
 
-def predict(var, regs):
-    city = var['縣市']
-    region = var['鄉鎮市區']
+    def outputReport(self):
+        path = self.defaultPath + "/report/"
+        city = self.quantizedVars["縣市"]
+        region = self.quantizedVars["鄉鎮市區"]
 
-    coefficient = regs[city][region]
-    y = numpy.dot(var["variables"], coefficient)
-    return y
-
-
-def outputReport(city, region):
-    path = "./regression_output/report/"
-    if os.path.isfile(path + city):
-        path += city + "/"
-        if os.path.isfile(path + region):
-            path += region + "/" + region + ".txt"
+        if os.path.isfile(path + city):
+            path += city + "/"
+            if os.path.isfile(path + region):
+                path += region + "/" + region + ".txt"
+            else:
+                path += city + ".txt"
         else:
-            path += city + ".txt"
-    else:
-        path += "total.txt"
-    f = open(path)
-    print (f.read())
-    f.close()
+            path += "total.txt"
+        f = open(path)
+        print (f.read())
+        f.close()
+
 
 if __name__ == '__main__':
-    if yesNoQuery("Input from file? [y/n]"):
+    ui = userInterface()
+
+    if ui.yesNoQuery("Input from file? [y/n]"):
         path = input("Please input path of your file: ")
-        inputVars = fileInput(path)
+        ui.fileInput(path)
     else:
-        inputVars = userInput()
+        ui.userInput()
 
-    quantizedVars = quantize(inputVars)
+    ui.quantize().predict()
+    print ("The predicted price is : %f\n" % ui.getPredictedPrice())
 
-    regressions = readRegressions()
-    predictedPrice = predict(quantizedVars, regressions)
-    print ("The predicted price is : %f\n" % predictedPrice)
-
-    if yesNoQuery("Output regression report? [y/n]"):
-        outputReport(quantizedVars['縣市'], quantizedVars['鄉鎮市區'])
+    if ui.yesNoQuery("Output regression report? [y/n]"):
+        ui.outputReport()
